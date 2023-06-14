04811682b03925c963851804654bbc12ea904a5c
UesugiErii <3022790047@qq.com>
2023-06-14 15:56:25 +0800

    更换头像

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://img.syt5.com/2020/0528/20200528015920753.jpg',
+      avatar: 'https://github.com/uesugieriislf/claude-web/blob/main/src/assets/avatar2.jpg',
       name: 'uesugieriislf',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
     },
